94229e6b81126e44b74c9bc08d86fe13ba05573b
Matt Hernandez <matt@modulus.io>
2014-01-14 21:36:29 -0500

    Moved the views into the server.

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var express = require('express')
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine', 'ejs');
 app.set('title', 'Node Mavens');
 
