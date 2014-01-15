04a3effcf5209338f0049d135366a5224aaa81c7
Matt Hernandez <matt@modulus.io>
2014-01-14 21:38:47 -0500

    Use secret from environment.

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
+app.use(express.cookieParser(process.env.SECRET || 'cats'));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
