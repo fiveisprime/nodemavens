7a5ab1306c465c50c766250025a322c76dc0b5ab
Matt Hernandez <matt@modulus.io>
2014-01-15 21:32:47 -0500

    About is now a child modal.
    Removed session storage; cleaned up the child initialization logic; added a
    helper to center elements.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -33,12 +33,29 @@ require([
 function($, Backbone, _, Handlebars) {
   'use strict';
 
+  $.fn.center = function () {
+    this.css('left', Math.max(
+      0,
+      (($(window).width() - $(this).outerWidth()) / 2) + $(window).scrollLeft()) + 'px');
+    return this;
+  };
+
   var App = {};
-  App.Models = {};
   App.Views = {};
+  App.Models = {};
+  App.Collections = {};
+
+  App.Models.Maven = Backbone.Model.extend({
+    defaults: {}
+  });
+
+  App.Collections.Mavens = Backbone.Collection.extend({
+    model: App.Models.Maven
+  });
 
   App.Views.Love = Backbone.View.extend({
     template: Handlebars.templates.love,
+    el: '#love-form',
     events: {
       'click [type=submit]': 'submit'
     },
@@ -55,20 +72,42 @@ function($, Backbone, _, Handlebars) {
     }
   });
 
+  App.Views.About = Backbone.View.extend({
+    template: Handlebars.templates.about,
+    el: '#about-modal',
+    events: {
+      'click': 'hide'
+    },
+    render: function() {
+      this.$el.html(this.template());
+      this.$el.center().fadeIn(100);
+    },
+    hide: function() {
+      this.$el.fadeOut(200);
+    }
+  });
+
   App.Views.Index = Backbone.View.extend({
     template: Handlebars.templates.index,
     el: document.body,
     events: {
-      'click #love': 'spreadLove'
+      'click #love': 'spreadLove',
+      'click #about': 'showAbout'
     },
     initialize: function() {
       this.render();
-      this.loveForm = new App.Views.Love({
-        el: $('#love-form')
-      });
+    },
+    initializeChildren: function() {
+      this.aboutForm = new App.Views.About();
+      this.loveForm = new App.Views.Love();
     },
     render: function() {
       this.$el.html(this.template());
+      _.defer(_.bind(this.initializeChildren, this));
+    },
+    showAbout: function() {
+      this.aboutForm.render();
+      return false;
     },
     spreadLove: function() {
       this.loveForm.render();
@@ -76,27 +115,12 @@ function($, Backbone, _, Handlebars) {
     }
   });
 
-  App.Views.About = Backbone.View.extend({
-    template: Handlebars.templates.about,
-    el: document.body,
-    initialize: function() {
-      this.render();
-    },
-    render: function() {
-      this.$el.html(this.template());
-    }
-  });
-
   App.Router = Backbone.Router.extend({
     routes: {
-      '': 'index',
-      'about': 'about'
+      '': 'index'
     },
     index: function() {
       new App.Views.Index();
-    },
-    about: function() {
-      new App.Views.About();
     }
   });
 
